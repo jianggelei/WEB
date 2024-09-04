145cff4c1db03cca96e35259ab9a9daff629025a
jianggelei <2937819275@qq.com>
2024-09-04 17:16:53 +0800

    1

---
+++ order-service/src/test/java/OrderStatus.java
@@ -0,0 +1,9 @@
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    CANCELLED
+
+
+
+}
---
+++ user-services/src/main/java/com/example/userservices/UserController/User.java
@@ -0,0 +1,4 @@
+package com.example.userservices.UserController;
+
+public record User(String username, String email) { }
+
