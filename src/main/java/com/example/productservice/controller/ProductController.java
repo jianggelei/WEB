76b10d232fb894706b9e9045d2a5231df97da60e
jianggelei <2937819275@qq.com>
2024-09-04 17:16:53 +0800

    1

---
+++ src/main/java/com/example/productservice/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.example.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @GetMapping("/product")
+   public String getProduct(@RequestParam String productId) {
+        return "Product : " + productId;
+    }
+}
